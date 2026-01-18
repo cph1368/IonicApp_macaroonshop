fea79b0a36254f0140995921cbca0978bdb943c6
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-18 20:54:05 +1100

    add redirect home by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,47 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // When open http://localhost:8100/ go to /tabs
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
+
 
   {
     path: 'settings',
-    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+    loadComponent: () =>
+      import('./settings/settings.page').then(m => m.SettingsPage),
   },
-  {
-    path: 'settings',
-    loadChildren: () =>
-      import('./settings/settings.module').then(m => m.SettingsPageModule),
-  },
+
   {
     path: 'tabs',
-    loadComponent: () => import('./pages/tabs/tabs.page').then(m => m.TabsPage),
+    loadComponent: () =>
+      import('./pages/tabs/tabs.page').then(m => m.TabsPage),
     children: [
-      { path: '', redirectTo: '/tabs/home', pathMatch: 'full' },
+      //  When you go to /tabs, go to /tabs/home
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+
       {
         path: 'home',
-        loadComponent: () => import('./pages/tabs/home/home.page').then( m => m.HomePage)
+        loadComponent: () =>
+          import('./pages/tabs/home/home.page').then(m => m.HomePage),
       },
       {
         path: 'explore',
-        loadComponent: () => import('./pages/tabs/explore/explore.page').then( m => m.ExplorePage)
+        loadComponent: () =>
+          import('./pages/tabs/explore/explore.page').then(m => m.ExplorePage),
       },
       {
         path: 'chats',
-        loadComponent: () => import('./pages/tabs/chats/chats.page').then( m => m.ChatsPage)
+        loadComponent: () =>
+          import('./pages/tabs/chats/chats.page').then(m => m.ChatsPage),
       },
       {
         path: 'account',
-        loadComponent: () => import('./pages/tabs/account/account.page').then( m => m.AccountPage)
-      } ],
+        loadComponent: () =>
+          import('./pages/tabs/account/account.page').then(m => m.AccountPage),
+      },
+    ],
   },
 
-
+  // optional safety net
+  { path: '**', redirectTo: 'tabs' },
 ];
