c4e32d10c26723677983cf71cfca7c13b84fbaa8
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-19 09:18:00 +1100

    add heading component

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonIcon, IonText, IonRow, IonCol, IonSearchbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { cart,chevronDownOutline, notificationsOutline } from 'ionicons/icons';
+import { cart,chevronDownOutline, notificationsOutline, optionsOutline } from 'ionicons/icons';
+import { ListHeadingComponent } from 'src/app/components/list-heading/list-heading.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [ IonHeader, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, IonButton, IonIcon, IonText, IonRow, IonCol, IonSearchbar]
+  imports: [ ListHeadingComponent, IonHeader, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, IonButton, IonIcon, IonText, IonRow, IonCol, IonSearchbar]
 })
 export class HomePage implements OnInit {
 
   constructor() { 
-    addIcons({ chevronDownOutline,cart,notificationsOutline });
+    addIcons({ chevronDownOutline,cart,notificationsOutline,optionsOutline });
   }
   gotoButton() {
     console.log('Button was clicked!');
