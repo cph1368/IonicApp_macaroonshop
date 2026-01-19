83145810ceef376d79c0acb1c6372cc47f56b2fe
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-19 22:59:52 +1100

    add new product list horizontal components + product components

---
+++ src/app/components/product-list-horizontal/product-list-horizontal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-horizontal',
+  templateUrl: './product-list-horizontal.component.html',
+  styleUrls: ['./product-list-horizontal.component.scss'],
+})
+export class ProductListHorizontalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
