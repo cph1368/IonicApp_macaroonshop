59d0cd21e0b3c1d8095029e93f1971c44780db06
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-20 02:44:39 +1100

    add product component pricing

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,14 @@
 import { Component, input, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product.interface';
 import { Strings } from 'src/enums/strings.enum';
-import { IonImg, IonThumbnail, IonCard } from "@ionic/angular/standalone";
+import { IonImg, IonThumbnail, IonCard, IonItem, IonLabel, IonText } from "@ionic/angular/standalone";
+import { VegNonvegIndicatorComponent } from "src/app/widgets/veg-nonveg-indicator/veg-nonveg-indicator.component";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
-  imports: [IonImg, IonThumbnail, IonCard],
+  imports: [IonImg, IonThumbnail, IonCard, IonItem, IonLabel, IonText, VegNonvegIndicatorComponent],
 })
 export class ProductComponent  implements OnInit {
 
