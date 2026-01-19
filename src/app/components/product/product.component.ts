13f464c13f0f333c91dc05b29c3a3b3a9b3e22e1
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-20 02:03:51 +1100

    add featured products #2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,13 @@
 import { Component, input, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product.interface';
 import { Strings } from 'src/enums/strings.enum';
+import { IonImg, IonThumbnail, IonCard } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
+  imports: [IonImg, IonThumbnail, IonCard],
 })
 export class ProductComponent  implements OnInit {
 
