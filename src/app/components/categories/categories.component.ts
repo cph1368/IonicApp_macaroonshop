4a4d0de1735e51ede2b15af3ba917224a708445d
cph1368 <31093206+cph1368@users.noreply.github.com>
2026-01-19 22:46:03 +1100

    add widgets

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,11 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, input, OnInit } from '@angular/core';
 import { Category } from 'src/app/interfaces/category.interface';
-import { IonRow, IonicSlides } from '@ionic/angular/standalone';
+import { IonRow, IonicSlides, IonCol } from '@ionic/angular/standalone';
+import { ImageCircleComponent } from 'src/app/widgets/image-circle/image-circle.component';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
-  imports: [],
+  imports: [IonCol, ImageCircleComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CategoriesComponent  implements OnInit {
